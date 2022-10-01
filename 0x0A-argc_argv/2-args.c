4040c79ea30d09713de978f868039ea445d6aaa8
OluTechie <johnsonoluwasegun19@gmail.com>
2022-09-30 17:07:04 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,11 +2,13 @@
 #include "stdlib.h>"
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - prints all the arguments a program receives.
  *
- * Return: Always 0 (Success)
+ * @argc: number of arguments
+ *
+ * @argv: array of arguments.
+ *
+ * Return: Always (0) success.
  */
 
 int main(int argc, char *argv[])
